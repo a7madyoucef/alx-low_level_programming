29fc1ec0751f7252572d798020288944669e6c76
a7madyoucef <a7md.joe1995@gmail.com>
2022-07-01 08:33:48 -0700

    pring a message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- /**
+/**
  *  main -Entary point
  *
  *  Return: Always 0 (Success)
@@ -10,3 +10,5 @@ int main(void) /* not expecting a return value */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
+/* printing a sentence */
