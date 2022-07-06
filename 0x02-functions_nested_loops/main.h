bbc17463e102e88580ca9cf6a0f1e4265eaada3d
a7madyoucef <a7md.joe1995@gmail.com>
2022-07-05 18:47:32 -0700

    a function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,8 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+void print_to_98(int n);
 
 #endif /* _main_ */
